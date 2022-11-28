88b335483082f0dc77a6f645d6562406723009ff
unknown <yuriba012@gmail.com>
2022-11-28 17:26:16 -0300

    Resolução dos exercicios de strings

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,11 @@
-//Crie a const para a frase aqui
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+
+let fraseNova = frase.replace("verde", "rosa")
+fraseNova = fraseNova.replace("azul", "laranja")
+
+console.log(`Frase: ${fraseNova}\n\nInclui verde? ${fraseNova.includes('verde')}
+Inclui laranja? ${fraseNova.includes('laranja')}`)
+
+fraseNova = fraseNova.replace('mas não deixe o gato sair', 'MAS NÃO DEIXE O GATO SAIR')
+
+console.log(fraseNova)
